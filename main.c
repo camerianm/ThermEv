18f8f8d4fd25fbe884ea65a5863c3048647e80c4
camerianm <camerianm@github.com>
2018-12-04 23:22:15 -0700

    Creates new output files for radiogenic heating and time-series analysis; enables naming convention to ease automation of param exploration

--- main.c
+++ main.c
@@ -4,85 +4,140 @@
 #include<stdlib.h>
 #include<math.h>
 
-int main(void)
+int main(int argc, char**argv)
 {
+	if (argc<4)
+	{
+		fprintf(stderr,"\n\nPlease re-try with arguments as follows:\n");
+		fprintf(stderr,"[executable] [int:Forward? 0or1] [dbl:UreyRatio] [dbl:beta]\n");
+		fprintf(stderr,"Example: thermev 0 0.72 0.30\n\n");
+		exit(10);
+	}
 
-	double dt_Ga=0.01;		//timestep in Ga
-	double gamma0=0.72;		//urey ratio, [H(t) / Q(t)]
-	double B=0.30;			//temp dependence of viscosity; may be variable
-	double E_kJmol=300.0;	//activation energy, kJ/mol; isoviscous=0, olivine=525
-							//300 per Korenaga ; 525 per McNamara
-	double Q0_TW=36.0;		//present day heat flux, TW (TJ/s)
-	double T0_C=1350.0;		//mantle potential temp OR average temp
-	double mass=1.00;		//planet mass (in earth masses); guides heat capacity
-	double radius=1.00;		//planet radius (in earth radii) - no role yet except grav.
-	int    forward=0;		//0 places t=0 at present, evolves back in time;
+	double 	urey=atof(argv[2]);		//urey ratio, [H(t) / Q(t)]
+	double 	B=atof(argv[3]);			//temp dependence of viscosity; may be variable
+	int    	forward=atoi(argv[1]);		//0 places t=0 at present, evolves back in time; cuts off at T>8000K.
 							//assumes Q0, H0, and T0 are present-day values.
 							//1 places t=0 at formation, evolves forward.
 							//note: possibly valid for stagnant-lid regimes;
 							//future updates may represent other regimes
-	double C_JK=7.0e27;		//heat capacity of whole earth in J/K
-	double t_Ga=0.0;			//start time in Ga
-	double t=t_Ga*3.1536e16;	//converts start time to seconds
-	double dt=dt_Ga*3.1536e16; 	//converts dt to seconds
-	double T0=T0_C+273;			//converts temp to K
-	double Q0=Q0_TW*1e12;		//converts flux to J/s
-	double H0=Q0*gamma0;		//initial heat production in J/s
-	double E=E_kJmol*1e3;		//converts activation energy to J/mol
-	double R=8.3145; 			//ideal gas constant in J/mol*K
+	char summary[50];
+	char timetemp[50];
+//	sprintf(summary,"summary_%d_%.2f_%.2f.dat",forward,urey,B);
+	sprintf(timetemp,"timetemp_%d_%.2f_%.2f.dat",forward,urey,B);
+	sprintf(summary,"summary.dat");
 
+	double E=300.0*1.0E3;		//activation energy, J/mol; isoviscous=0. 300*1.0E3 Korenaga ; 525*1.0E3 MacNamara
+	double Q0=3.6e13;		//present day heat flux, W (J/s)
+
+	double H0=Q0*urey;		//present heat production, W (J/s)
+	double T0_C=1350.0;		//mantle potential temp OR average temp
+	double C_JK=7.0e27;		//heat capacity of whole earth in J/K
+	double T0=T0_C+273.15;		//converts temp to K
+	double R=8.3145; 		//ideal gas constant in J/mol*K
+
+	double t_Ga=0.0, dt_Ga=0.005;	//timestep in Ga
+	double GatoS=3.1536e16;		//converts from Ga to seconds
+	double t=0.0, dt=dt_Ga*GatoS;
+	double T=T0, dT;
+
+	int i, iter=0;
+
+	double mass=1.00;		//planet mass (in earth masses); guides heat capacity
+	double radius=1.00;		//planet radius (in earth radii) - no role yet except grav.
 	double G=6.67408e-11, Re=6.371e6, Me=5.9722e24;
    	double grav=G*(Me*mass)/(pow((Re*radius),2));	//calculates surface gravity
-    double Cp=C_JK*mass;		//assumes heat capacity roughly scales with mass
+    	double Cp=C_JK*mass;		//assumes heat capacity roughly scales with mass
  
 	double h_n[4]={0.372, 0.0164, 0.430, 0.181}; // fraction of total heat production at t=0
-    double L_n[4]={0.155, 0.985, 0.0495, 0.555}; // lambda - decay/growth constant in 1/Gyr
-    double Qt, Ht, H_n1, H_n2, H_n3, H_n4 ; // total production; multiplier for each species
+	double L_n[4]={0.155, 0.985, 0.0495, 0.555}; // lambda - decay/growth constant in 1/Gyr
+    	double H_n[4], Qt, Ht=0.0; // total production; multiplier for each species
 
-	FILE*TimeTemp; TimeTemp=fopen("TimeTemp.txt", "w");
-	fprintf(TimeTemp, "%.2f, %.2f\n", t_Ga, T0_C);	//present time + temp in C
-	int iter=0;		//iteration counter
-	t_Ga=t_Ga+dt_Ga;				//setting time in Ga
-	double dT=((H0 - Q0)/C_JK)*dt;	//temp chg
+	FILE*ky; ky=fopen(summary,"w");
+	fprintf(ky,"timetemp.dat header info:\nCol.3-6 show fraction of H(t=0)\nCol.7-10 show fraction of H(t=time)\n\n");
+	fprintf(ky,"Col01: Time(GA)\nCol02: Temp(C)\nCol03: 238U\nCol04: 235U\nCol05: 232Th\nCol06: 40K\n");
+	fprintf(ky,"Col07: 238U/H(t)\nCol08: 235U/H(t)\nCol09: 232Th/H(t)\nCol10: 40K/H(t)\nCol11: H(t)/H0\nCol12: H(t)\nCol13: UreyRatio\n");
+	fprintf(ky,"\nregistry header info: #iterations filename\n");
 
-	if (forward==0)
+	FILE*fp; fp=fopen(timetemp,"w");
+	fprintf(fp,"%.3f %.3f %.5f %.5f %.5f %.5f", t_Ga, T0_C, h_n[0], h_n[1], h_n[2], h_n[3]);
+	fprintf(fp,"%.5f %.5f %.5f %.5f %.5f %.3e %.5f\n", h_n[0], h_n[1], h_n[2], h_n[3], H0/H0, H0, H0/Q0);
+
+
+	dT=((H0-Q0)/C_JK)*dt;	//Change in temp that should apply between timesteps 0 and 1
+	
+
+	if (forward==0)				//If we are running backward in time...
 	{
-		double T=T0-dT; t=t+dt;	// applying present temp chg linearly back, 1st timestep in S
-		fprintf(TimeTemp, "%.2f, %.2f\n", t_Ga, T-273);	//registers first timestep back
-		while (t_Ga<4.6)
+		while ((t_Ga<4.6) && (T<8000))	
 		{
-			iter=iter+1;
-			H_n1 = h_n[0]*exp(L_n[0]*t_Ga); // 238U	
-	        H_n2 = h_n[1]*exp(L_n[1]*t_Ga); // 235U
-	        H_n3 = h_n[2]*exp(L_n[2]*t_Ga); // 232Th
-	        H_n4 = h_n[3]*exp(L_n[3]*t_Ga); // 40K
-	        Ht = H0 * (H_n1 + H_n2 + H_n3 + H_n4);		//calculates H term
+			T=T-dT;				//Find out what mantle temp would be at timestep, to inform tdep visc.
+			t_Ga=t_Ga+dt_Ga;
+			t=t_Ga*GatoS;
+			fprintf(fp,"%.3f %.3f ", t_Ga, T-273.15); 	//Starts to log info @ timestep...
+
 	        Qt = Q0*pow((T/T0), (1+B))*pow((exp((E/R)*((1/T0)-(1/T)))), B);
-	        dT = ((Ht - Qt)/C_JK)*dt; 	//finding temp difference
-	        T = T - dT;					//applying temp difference
-	        t_Ga=t_Ga+dt_Ga;	//note: decay rate at time t_Ga is one timestep off.
-	        fprintf(TimeTemp, "%.2f, %.2f\n", t_Ga, T-273); 
+
+			for (i=0;i<4;i++)	//Calculate how heat production changes for each radionuclide, and total.
+			{
+				H_n[i]=h_n[i]*exp(L_n[i]*t_Ga);		//exponential growth - i.e. decay in reverse
+				Ht=Ht+H_n[i];
+				fprintf(fp,"%.5f ",H_n[i]);
+			}
+
+			for (i=0;i<4;i++)	//Calculate each radionuclide's fraction of total heating at time t
+			{
+				fprintf(fp,"%.5f ", H_n[i]/Ht);
+			}
+
+			fprintf(fp,"%.5f ",Ht);
+			Ht=Ht*H0;
+			fprintf(fp,"%.3e",Ht);
+			fprintf(fp,"%.5f\n",Ht/Qt);
+	        	dT=((Ht-Qt)/C_JK)*dt;		//find temp difference for next time step
+			iter++;
+			Ht=0.0;
 		}
 	}
 	else
 	{
-		double T=T0+dT; t=t+dt;	// applying present temp chg linearly forward, 1st timestep in S
-		fprintf(TimeTemp, "%.2f, %.2f\n", t_Ga, T-273);	//registers first timestep fwd
+		//h_n[4]={0.19447,0.21200,0.14951,0.44402};
 		while (t_Ga<4.6)
 		{
-			iter=iter+1;
-			H_n1 = h_n[0]*exp(-1*L_n[0]*t_Ga); // 238U	
-	        H_n2 = h_n[1]*exp(-1*L_n[1]*t_Ga); // 235U
-	        H_n3 = h_n[2]*exp(-1*L_n[2]*t_Ga); // 232Th
-	        H_n4 = h_n[3]*exp(-1*L_n[3]*t_Ga); // 40K
-	        Ht = H0 * (H_n1 + H_n2 + H_n3 + H_n4);		//calculates H term
+			T=T+dT;				//Find out what mantle temp would be at timestep, to inform tdep visc.
+			t_Ga=t_Ga+dt_Ga;
+			t=t_Ga*GatoS;
+			fprintf(fp,"%.3f %.3f ", t_Ga, T-273.15); 	//Starts to log info @ timestep...
+
 	        Qt = Q0*pow((T/T0), (1+B))*pow((exp((E/R)*((1/T0)-(1/T)))), B);
-	        dT = ((Ht - Qt)/C_JK)*dt; 	//finding temp difference
-	        T = T + dT;					//applying temp difference
-	        t_Ga=t_Ga+dt_Ga;	//note: decay rate at time t_Ga is one timestep off.
-	        fprintf(TimeTemp, "%.2f, %.2f\n", t_Ga, T-273); 
+
+			for (i=0;i<4;i++)	//Calculate how heat production changes for each radionuclide, and total.
+			{
+				H_n[i]=h_n[i]*exp(-1*L_n[i]*t_Ga);	//exponential decay
+				Ht=Ht+H_n[i];
+				fprintf(fp,"%.5f ",H_n[i]);
+			}
+
+			for (i=0;i<4;i++)	//Calculate each radionuclide's fraction of total heating at time t
+			{
+				fprintf(fp,"%.5f ", H_n[i]/Ht);
+			}
+
+			fprintf(fp,"%.5f ",Ht);
+			Ht=Ht*H0;
+			fprintf(fp,"%.3e ",Ht);
+			fprintf(fp,"%.5f\n", Ht/Qt);
+	        	dT=((Ht-Qt)/C_JK)*dt;		//find temp difference for next time step
+			iter++;
+			Ht=0.0;
 	    }
 	}
-	fprintf(stdout, "%d iterations displayed in TimeTemp.txt.\n", iter+1);
+
+FILE*reg; reg=fopen("registry.dat","a");
+fprintf(reg,"%d %d %.3f %.3f timetemp_%d_%.2f_%.2f.dat\n", iter+1,forward, urey, B, forward,urey,B);
+fprintf(stdout,"Done with Forward=%d\tUrey=%.3f\tBeta=%.3f\n", forward, urey, B);
+fclose(ky);
+fclose(fp);
+fclose(reg);
 return 0;
 }
